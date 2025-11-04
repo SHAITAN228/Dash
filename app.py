330c5add7b371199edc5284350cf4721b97a4b9b
SHAITAN228 <106577800+SHAITAN228@users.noreply.github.com>
2025-11-05 02:24:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 
-
+server = app.server
 
 app.layout = html.Div([
 
@@ -190,4 +190,5 @@ def update_pie_graph(selected_year):
 
 if __name__ == '__main__':
     
+
     app.run(debug=True)
